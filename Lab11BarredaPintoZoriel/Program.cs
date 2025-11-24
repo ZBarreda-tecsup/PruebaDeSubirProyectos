b8295865db05423c85a0d17560a7dada0fd037ca
Barreda Pinto Zoriel Shanty <zoriel.barreda@tecsup.edu.pe>
2025-11-23 20:43:01 -0500

    progam changes

--- Lab11BarredaPintoZoriel/Program.cs
+++ Lab11BarredaPintoZoriel/Program.cs
@@ -18,8 +18,7 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
